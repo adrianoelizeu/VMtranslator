e860b41517968cf9a43ccad2f988045f931fa54f
Adriano Elizeu <adrianoelizeu@msn.com>
2023-11-14 13:55:58 -0300

    0perações (aritméticas, lógicas, push, pop)

--- Command.java
+++ Command.java
@@ -1,3 +1,23 @@
+
 public class Command {
-    
+
+    public enum Type {
+        ADD, 
+        SUB,
+        NEG,
+        EQ,
+        GT,
+        LT,
+        AND,
+        OR,
+        NOT,
+        PUSH,
+        POP,
+        LABEL,
+        GOTO,
+        IF,
+        RETURN,
+        CALL,
+        FUNCTION;
+    }
 }
