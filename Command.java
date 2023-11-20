a178d9456258de7f27386e6cd0af0e691c85d297
Adriano Elizeu <adrianoelizeu@msn.com>
2023-11-20 10:52:52 -0300

    Implementação do nextCommand

--- Command.java
+++ Command.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Command {
 
@@ -19,5 +21,24 @@ public class Command {
         RETURN,
         CALL,
         FUNCTION;
+    }
+
+    
+    public Command.Type type;
+    public List<String> args = new ArrayList<>();
+
+    public Command (String[] command) {
+
+        if (command[0].equals("if-goto"))
+            type = Type.IF;
+        else
+            type = Command.Type.valueOf(command[0].toUpperCase());
+
+        for (int i=1;i<command.length;i++){
+            var arg = command[i];
+            var pos = arg.indexOf("//");
+            if (pos != -1) arg = arg.substring(0, pos);
+            args.add(arg.strip());
+        } 
     }
 }
